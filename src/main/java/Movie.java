a7783a0462df721578ec86baabf27bec9e1801a6
pawel <pawelpieniak88@gmail.com>
2022-11-26 17:05:07 +0100

    NewProject

---
+++ src/main/java/Movie.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+
+    String title;
+    Director director;
+    String genre;
+    int year;
+    List<Actor> actors;
+
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    String firstName;
+    String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
